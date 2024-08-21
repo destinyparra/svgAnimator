3a14dbdb9e211c0bc6cac418f44524b8fec30d6b
Destiny Parra <116921929+DestNp@users.noreply.github.com>
2024-08-21 00:14:32 -0700

    omg we r getting somewhere
    transforms from apple to bed
    - needed to run on localhost instead of trying to load SVG files from local file system using the file:// protocol, which is not allowed due to security restrictions :(
    
    - the animation looks a tad funky but its honest work :thumbsup:

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,27 +8,27 @@ function loadSVG(path, callback) {
 }
 
 var bedPoints, applePoints;
-var simpleCup, fancyCup;
+var bed, apple;
 
 loadSVG('assets/svgs/1apple.svg', function (node) {
-    simpleCup = Snap.select('#apple');
-    bedPoints = simpleCup.node.getAttribute('d');
+    bed = Snap.select('#apple');
+    bedPoints = bed.node.getAttribute('d');
 });
 
 loadSVG('assets/svgs/2bed.svg', function (node) {
-    fancyCup = Snap.select('#bed');
-    applePoints = fancyCup.node.getAttribute('d');
+    apple = Snap.select('#bed');
+    applePoints = apple.node.getAttribute('d');
 
     animateSVG(); // Start animation after both SVGs are loaded
 });
 
 function animateSVG() {
     var toApple = function () {
-        simpleCup.animate({ d: applePoints }, 1000, mina.backout, toBed);
+        bed.animate({ d: applePoints }, 1000, mina.backout, toBed);
     }
 
     var toBed = function () {
-        simpleCup.animate({ d: bedPoints }, 1000, mina.backout, toApple);
+        bed.animate({ d: bedPoints }, 1000, mina.backout, toApple);
     }
 
     toBed(); // Initiate the animation loop
