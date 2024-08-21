029568a947d173eb403099bf7819de6ee637ef2f
Destiny Parra <116921929+DestNp@users.noreply.github.com>
2024-08-20 23:47:55 -0700

    forgot directory assets in path

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,12 @@ function loadSVG(path, callback) {
 var bedPoints, applePoints;
 var simpleCup, fancyCup;
 
-loadSVG('svgs/1apple.svg', function (node) {
+loadSVG('assets/svgs/1apple.svg', function (node) {
     simpleCup = Snap.select('#apple');
     bedPoints = simpleCup.node.getAttribute('d');
 });
 
-loadSVG('svgs/2bed.svg', function (node) {
+loadSVG('assets/svgs/2bed.svg', function (node) {
     fancyCup = Snap.select('#bed');
     applePoints = fancyCup.node.getAttribute('d');
 
