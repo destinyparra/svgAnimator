e1961aa643d846fdfc52e9e6689ecc61a1afcd32
Destiny Parra <116921929+DestNp@users.noreply.github.com>
2024-08-20 23:44:18 -0700

    test1

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+var svgContainer = document.getElementById('icons');
+
+function loadSVG(path, callback) {
+    Snap.load(path, function (f) {
+        svgContainer.appendChild(f.node);
+        callback(f.node);
+    });
+}
+
+var bedPoints, applePoints;
+var simpleCup, fancyCup;
+
+loadSVG('svgs/1apple.svg', function (node) {
+    simpleCup = Snap.select('#apple');
+    bedPoints = simpleCup.node.getAttribute('d');
+});
+
+loadSVG('svgs/2bed.svg', function (node) {
+    fancyCup = Snap.select('#bed');
+    applePoints = fancyCup.node.getAttribute('d');
+
+    animateSVG(); // Start animation after both SVGs are loaded
+});
+
+function animateSVG() {
+    var toApple = function () {
+        simpleCup.animate({ d: applePoints }, 1000, mina.backout, toBed);
+    }
+
+    var toBed = function () {
+        simpleCup.animate({ d: bedPoints }, 1000, mina.backout, toApple);
+    }
+
+    toBed(); // Initiate the animation loop
+}
