b4a7fe04f7eb62b1957dab7d1d90a1fd99745e1b
Destiny Parra <116921929+DestNp@users.noreply.github.com>
2024-08-22 01:52:12 -0700

    updates
    added 3 svgs to cycle through
    added favicon

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,29 +7,53 @@ function loadSVG(path, callback) {
     });
 }
 
-var bedPoints, applePoints;
-var bed, apple;
+var bedPoints, applePoints, bookPoints;
+var bed, apple, book;
 
 loadSVG('assets/svgs/1apple.svg', function (node) {
-    bed = Snap.select('#apple');
+    apple = Snap.select('#apple');
+    applePoints = apple.node.getAttribute('d');
+});
+loadSVG('assets/svgs/2bed.svg', function (node) {
+    bed = Snap.select('#bed');
     bedPoints = bed.node.getAttribute('d');
 });
 
-loadSVG('assets/svgs/2bed.svg', function (node) {
-    apple = Snap.select('#bed');
-    applePoints = apple.node.getAttribute('d');
+loadSVG('assets/svgs/4book.svg', function (node) {
+    book = Snap.select('#book');
+    bookPoints = book.node.getAttribute('d');
 
     animateSVG(); // Start animation after both SVGs are loaded
 });
 
 function animateSVG() {
+
+
     var toApple = function () {
-        bed.animate({ d: applePoints }, 1000, mina.backout, toBed);
+        apple.animate({ d: applePoints }, 1000, mina.easein, function () {
+            setTimeout(toBed, 500); // Pause for 500ms before animating back
+        })
+
     }
 
     var toBed = function () {
-        bed.animate({ d: bedPoints }, 1000, mina.backout, toApple);
+        bed.animate({ d: bedPoints }, 1000, mina.easein, function () {
+            setTimeout(toBook, 500);
+        });
+
     }
 
-    toBed(); // Initiate the animation loop
+
+    var toBook = function () {
+        book.animate({ d: bookPoints }, 1000, mina.easein, function () {
+            setTimeout(toApple, 500)
+        })
+    }
+
+    toApple(); // Initiate the animation loop
+
 }
+
+
+
+//check svg files from figma ig linear gradient is correct and fill="url()"
